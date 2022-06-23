20847a487d4e7bb1784f46ec568cbae66aa3b607
s-sunkum <sunkum.2@osu.edu>
2022-06-22 21:09:30 -0400

    Testing initial deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a test
         </a>
       </header>
     </div>
