ac947a9d637c77c423085dd359f373dc86c97642
s-sunkum <sunkum.2@osu.edu>
2022-06-22 21:02:26 -0400

    Updates

---
+++ precache-manifest.4820798f5fb95dc1803f615ea0e6d220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f783a66a4bdae02c4db",
+    "url": "/myPortfolio/static/css/main.94f03d43.chunk.css"
+  },
+  {
+    "revision": "1f783a66a4bdae02c4db",
+    "url": "/myPortfolio/static/js/main.1f783a66.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/myPortfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/myPortfolio/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "6ddd82b69e6a4efd0ae0",
+    "url": "/myPortfolio/static/js/runtime~main.6ddd82b6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/myPortfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "068f296e1cd5574a659bda7cbe8d2b62",
+    "url": "/myPortfolio/index.html"
+  }
+];
