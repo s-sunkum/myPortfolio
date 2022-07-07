beaae9a8c04f4181b4727050a1f26cbe0f2ee30c
s-sunkum <sunkum.2@osu.edu>
2022-07-07 16:45:32 -0400

    Added hamburger menu to top right

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Topbar from "./components/Topbar/Topbar";
 import Intro from "./components/Intro/intro.jsx";
 import Portfolio from "./components/Portfolio/portfolio.jsx";
 import Contact from "./components/Contact/contact.jsx";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="App">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,29 +3,31 @@ import "./Topbar.scss";
 import "material-icons/iconfont/material-icons.css";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
-class Topbar extends Component {
-  render() {
-    return (
-      <div class="topbar">
-        <div className="wrapper">
-          <div className="left">
-            <a href="#intro" className="logo">
-              Shravun
-            </a>
-            <div className="itemContainer">
-              <PersonPinIcon className="icon" />
-              <span>(614)805-6711</span>
-            </div>
-            <div className="itemContainer">
-              <MailOutlineIcon className="icon" />
-              <span>shravun99@gmail.com</span>
-            </div>
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div class={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Shravun
+          </a>
+          <div className="itemContainer">
+            <PersonPinIcon className="icon" />
+            <span>(614)805-6711</span>
           </div>
-          <h1>test</h1>
-          <div className="right"></div>
+          <div className="itemContainer">
+            <MailOutlineIcon className="icon" />
+            <span>sunkum.2@osu.edu</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-export default Topbar;
