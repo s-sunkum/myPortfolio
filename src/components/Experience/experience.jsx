046e2f0138997d4cb5e6c1e49d5a9942bd8b08e2
s-sunkum <sunkum.2@osu.edu>
2022-07-11 00:24:18 -0400

    Worked on pictures for timeline and github/linkedin icons

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -16,40 +16,57 @@ export default function Experience() {
           iconStyle={{ background: "#280d5a", color: "#fff" }}
           icon={<SchoolIcon></SchoolIcon>}
         >
-          <h3 className="vertical-timeline-element-title">
-            Jerome High School
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Dublin, Ohio</h4>
-          <p>High School Diploma</p>
-          <p>Date: 2016 - 2020</p>
+          <div className="column1">
+            <h3 className="vertical-timeline-element-title">
+              Jerome High School
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Dublin, Ohio</h4>
+            <p>High School Diploma</p>
+            <p>Date: 2016 - 2020</p>
+          </div>
+          <div className="column2">
+            <img src={require("./Knot.png")} />
+          </div>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           iconStyle={{ background: "#280d5a", color: "#fff" }}
           icon={<WorkIcon></WorkIcon>}
         >
-          <h3 className="vertical-timeline-element-title">
-            Software Engineer Intern - Rovisys
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Aurora, Ohio</h4>
-          <p>
-            Developed internal applications for Enterprise Web Development Team
-            and operated data collection procedures for Industrial Artificial
-            Intelligence division.
-          </p>
-          <p>Date: May 2022 - August 2022</p>
+          <div className="column1">
+            <h3 className="vertical-timeline-element-title">
+              Software Engineer Intern - Rovisys
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Aurora, Ohio</h4>
+            <p>
+              Developed internal applications for Enterprise Web Development
+              Team and operated data collection procedures for Industrial
+              Artificial Intelligence division.
+            </p>
+            <p>Date: May 2022 - August 2022</p>
+          </div>
+          <div className="column2">
+            <img src={require("./rovisys.png")} />
+          </div>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           iconStyle={{ background: "#280d5a", color: "#fff" }}
           icon={<SchoolIcon></SchoolIcon>}
         >
-          <h3 className="vertical-timeline-element-title">
-            The Ohio State University
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Columbus, Ohio</h4>
-          <p>Bachelor's Degree in Computer Science</p>
-          <p>2020 - Present</p>
+          <div className="column1">
+            <h3 className="vertical-timeline-element-title">
+              The Ohio State University
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Columbus, Ohio
+            </h4>
+            <p>Bachelor's Degree in Computer Science</p>
+            <p>2020 - Present</p>
+          </div>
+          <div className="column2">
+            <img src={require("./osu.png")} />
+          </div>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import "./Topbar.scss";
 import "material-icons/iconfont/material-icons.css";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import EmailIcon from "@mui/icons-material/Email";
 import HomeIcon from "@mui/icons-material/Home";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
@@ -14,11 +16,19 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </a>
           <div className="itemContainer">
             <PersonPinIcon className="icon" />
-            <span>(614)805-6711</span>
+            <span>(614)-805-6711</span>
           </div>
           <div className="itemContainer">
-            <MailOutlineIcon className="icon" />
+            <EmailIcon className="icon" />
             <span>sunkum.2@osu.edu</span>
+          </div>
+          <div className="itemContainer">
+            <LinkedInIcon className="icon" />
+            <a href="https://www.linkedin.com/in/shravun-sunkum-ab13371b7/">
+              Linkedin
+            </a>
+            <GitHubIcon className="icon" />
+            <a href="https://github.com/s-sunkum">Github</a>
           </div>
         </div>
         <div className="right">
